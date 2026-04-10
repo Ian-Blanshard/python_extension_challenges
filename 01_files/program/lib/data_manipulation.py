09ff334f8e9aa4556b8117e9395b195d4191bd51
ian blanshard <ian246@hotmail.co.uk>
2026-04-10 16:13:17 +0100

    challenges complete

--- 01_files/program/lib/data_manipulation.py
+++ 01_files/program/lib/data_manipulation.py
@@ -24,8 +24,11 @@ import os
 # Notes:
 # * Use the already imported "os" module to check whether a given filename exists
 def does_file_exist(filename):
-    pass
-
+    if os.path.exists(filename):
+        return True
+    else:
+        return False
+    
 # Purpose: get the contents of a given file and return them; if the file cannot be
 # found, return a nice error message instead
 # Example:
@@ -42,7 +45,11 @@ def does_file_exist(filename):
 # * Use readlines() to read the contents
 # * Use should use does_file_exist()
 def get_file_contents(filename):
-    pass
+    if does_file_exist(filename):
+        with open(filename, 'r', encoding='utf-8') as file:
+            return file.readlines()
+    else:
+        return "This file cannot be found!"
 
 # Purpose: fetch Christmas Day (25th December) air quality data rows, and if
 # boolean argument "include_header_row" is True, return the first header row
@@ -61,7 +68,46 @@ def get_file_contents(filename):
 # * should use get_file_contents() - N.B. as should any subsequent
 # functions you write, using anything previously built if and where necessary
 def christmas_day_air_quality(filename, include_header_row):
-    pass
+    file = get_file_contents(filename)
+    data = []
+    if include_header_row:
+        data.append(file[0])
+    for row in file:
+        if row[0:10] == '25/12/2004':
+            data.append(row)
+    return data
+#####################################################################
+######## my own function to process data into a useable format ######
+#####################################################################
+def process_data(filename):
+    """
+    returns dictionaries for each line of data 
+    nested into an array
+    """
+    # open file and read contents
+    file = get_file_contents(filename)
+
+    # list to hold row data dictionaries
+    processed_data = []
+
+    # create list of headers
+    headers = file[0].split(';')
+    headers = headers[:-2]
+    
+    # create list for each row of data
+    data_lists = []
+    for row in file[1:]:
+        seperate_data = row.split(';')
+        seperate_data = seperate_data[:-2]   
+        data_lists.append(seperate_data)
+    
+    # create dict for each row and add to processed data array
+    for row in data_lists:
+        row_dict = {k: v for k, v in zip(headers,row)}
+        processed_data.append(row_dict)
+    # function returns processed data
+    return processed_data
+ 
 
 # Purpose: fetch Christmas Day average of "PT08.S1(CO)" values to 2 decimal places
 # Example:
@@ -71,7 +117,17 @@ def christmas_day_air_quality(filename, include_header_row):
 # Date;Time;CO(GT);PT08.S1(CO);NMHC(GT);C6H6(GT);PT08.S2(NMHC);NOx(GT);PT08.S3(NOx);NO2(GT);PT08.S4(NO2);PT08.S5(O3);T;RH;AH;;
 # 10/03/2004;18.00.00;2,6;1360;150;11,9;1046;166;1056;113;1692;1268;13,6;48,9;0,7578;;
 def christmas_day_average_air_quality(filename):
-    pass
+
+    data = process_data(filename)
+    # filter all rows from christmas
+    christmas = list(filter(lambda x: x['Date'][0:5] == '25/12', data))
+    # create list of all the co values from christmas row
+    christmas_co = [int(x['PT08.S1(CO)']) for x in christmas]
+    # return average of co values 
+    return round(sum(christmas_co) / len(christmas_co), 2)
+    
+    
+    
 
 # Purpose: scrape all the data and calculate average values for each of the 12 months
 #          for the "PT08.S1(CO)" values, returning a dictionary of keys as integer
@@ -82,7 +138,22 @@ def christmas_day_average_air_quality(filename):
 # Notes:
 # * Data from months across multiple years should all be averaged together
 def get_averages_for_month(filename):
-    pass
+
+    data = process_data(filename)
+
+    months = {}
+
+    for i in range(1,13):
+
+        if i < 10:
+            i = '0' + str(i)
+        else:
+            i = str(i)
+
+        month = list(filter(lambda x: x['Date'][3:5] == i, data))
+        months[int(i)] = round(sum([int(x['PT08.S1(CO)']) for x in month]) / len(month),2)
+    
+    return months
 
 # Purpose: write only the rows relating to March (any year) to a new file, in the same
 # location as the original, including the header row of labels
@@ -91,7 +162,27 @@ def get_averages_for_month(filename):
 #   Returns: nothing, but writes header + March data to file called
 #            "AirQualityMarch.csv" in same directory as "AirQuality.csv"
 def create_march_data(filename):
-    pass
+    my_csv = ''
+
+    # get header
+    data = get_file_contents(filename)
+    my_csv += data[0]
+
+    # get march data
+    data = process_data(filename)
+    march = list(filter(lambda x: x['Date'][3:5] == '03', data))
+    
+    # add march data to csv string
+    for d in march:
+        line = ''
+        for v in d.values():
+            line += f'{v};'
+        my_csv += line + ';\n'
+    
+    # write data to file
+    f = open('AirQualityMarch.csv', 'w')
+    f.write(my_csv)
+    
 
 # Purpose: write monthly responses files to a new directory called "monthly_responses",
 # in the same location as AirQuality.csv, each using the name format "mm-yyyy.csv",
@@ -101,4 +192,44 @@ def create_march_data(filename):
 #   Returns: nothing, but files such as monthly_responses/05-2004.csv exist containing
 #            data matching responses from that month and year
 def create_monthly_responses(filename):
-    pass
+
+    # create dir, if exists do nothing
+    os.makedirs('monthly_responses', exist_ok=True)
+
+    def build_csv(month_dictionary):
+        """
+        when passed a dict containing a months
+        data build the csv and return it
+        """
+        # get header
+        data = get_file_contents(filename)
+        month_csv = data[0]
+        # add month data to csv string
+        for d in month_dictionary:
+            line = ''
+            for v in d.values():
+                line += f'{v};'
+            month_csv += line + ';\n'
+        # return built csv
+        return month_csv
+
+
+    # get/process data
+    data = process_data(filename)
+    #loop through each year 2004/2005
+    for year in range(2004,2006):
+        # filter data for loops current year
+        years_data = list(filter(lambda x: x['Date'][6:10] == str(year), data))
+        # loop through that years data and create csv for each month
+        for month in range(1,13):
+            if month < 10:
+                month = '0' + str(month)
+            else:
+                month = str(month)
+            month_dict = list(filter(lambda x: x['Date'][3:5] == month, years_data))
+            # if there is data for the month build csv and write to dir
+            if month_dict:
+                month_csv = build_csv(month_dict)
+                with open(f'monthly_responses/{month}-{year}.csv', 'w') as f:
+                    f.write(month_csv)
+
--- 02_json/program/lib/json_load.py
+++ 02_json/program/lib/json_load.py
@@ -1,82 +1,53 @@
-# == INSTRUCTIONS ==
-#
-# Below, you'll find lots of incomplete functions.
-#
-# Your job: Implement each function so that it does its job effectively.
-#
-# * Use the material, Python Docs and Google as much as you want
-
 # == EXERCISES ==
-
+from urllib.request import urlopen
+import json
 # Purpose: Use Python libraries to request the provided URL, convert the
 #          response data to JSON, and return the data.
-# Example:
-#   Call:    load_data_from_url("https://example.org/my.json")
-#   Returns: A JSON object
 def load_data_from_url(url):
-    pass
-
+    return json.loads(urlopen(url).read().decode('UTF-8'))
+    
 # Purpose: Use Python libraries to open the specified file, convert the
 #          data to JSON, and return the data.
-# Example:
-#   Call:    load_data_from_file("my_test_data.json")
-#   Returns: A JSON object
 def load_data_from_file(filename):
-    pass
-
+    with open(filename) as file:
+        return json.load(file)
+    
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           directed by the named person.
-# Example:
-#   Call:    get_films_by_director("my_test_data.json", "Olivia Wilde")
-#   Returns: ["Booksmart, "Don't Worry Darling"]
-def get_films_by_director(filename, director):
-    pass
+def get_films_by_director(filename, director):    
+    return [x['name'] for x in load_data_from_file(filename) if x['director'] == director]
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           starring the named person.
-# Example:
-#   Call:    get_films_by_actor("my_test_data.json", "Dwayne Johnson")
-#   Returns: ["Jumanji", "Jungle Cruise"]
 def get_films_by_actor(filename, desired_actor):
-    pass
+    return [x['name'] for x in load_data_from_file(filename) if desired_actor in x['stars']]
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           with a rating which is AT LEAST the value specified.
-# Example:
-#   Call:    get_films_with_minimum_rating("test.json", 9.3)
-#   Returns: ["The Shawshank Redemption"]
 def get_films_with_minimum_rating(filename, rating):
-    pass 
+    return [x['name'] for x in load_data_from_file(filename) if x['imdb_rating'] >= rating]
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           which were released during the specified years.
-# Example:
-#   Call:    get_films_within_year_range("my_test_data.json", 1994, 1996)
-#   Returns: ["The Lion King", "Independence Day"]
 def get_films_within_year_range(filename, start_year, end_year):
-    pass
+    return [x['name'] for x in load_data_from_file(filename) if x['year'] >= start_year and x['year'] <= end_year]
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           in order of the year that they were released.
-# Example:
-#   Call:    order_films_chronologically("test.json")
-#   Returns: ["12 Angry Men", "The Godfather", "The Godfather: Part II", ... ]
 def order_films_chronologically(filename):
-    pass
+    return [x['name'] for x in sorted(load_data_from_file(filename), key=lambda x: x['year'])]
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           starting with the most recent.
-# Example:
-#   Call:    order_films_most_recent_first("test.json")
-#   Returns: ["The Dark Knight", "The Shawshank Redemption", "The Godfather: Part II", ... ]
 def order_films_most_recent_first(filename):
-    pass
+    return [x['name'] for x in sorted(load_data_from_file(filename), key=lambda x: x['year'], reverse=True)]
 
 # Purpose: Load the sample JSON from file, and returns a deduplicated list 
 #           of all the actors whose name begins with that letter,
 #           in alphabetical order.
-# Example:
-#   Call:    all_actors_starting_with_letter("test.json", "a")
-#   Returns: ["Aaron Eckhart, "Al Pacino"]
 def all_actors_starting_with_letter(filename, letter):
-    pass
+    
+    stars = [star for film in load_data_from_file(filename) for star in film['stars'] if star[0].lower() == letter]
+    stars = list(set(stars))
+    stars.sort()    
+    return (stars)
